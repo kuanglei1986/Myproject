0ea2989463f5e27088319b86e8816d8dc24d42ea
kuanglei1986 <344960239@qq.com>
2020-12-13 16:55:49 +0800

    servlet example

---
+++ out/artifacts/Myproject_war_exploded/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: admin
+  Date: 2020/12/13
+  Time: 11:31
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>kuanglei</title>
+  </head>
+  <body>
+  helloworld1
+  <a href="MyServlet">MyServlet</a>
+  </body>
+</html>
---
+++ src/com/kl/servlet/MyServlet.java
@@ -0,0 +1,15 @@
+package com.kl.servlet;
+
+import java.io.IOException;
+
+@javax.servlet.annotation.WebServlet("/MyServlet")
+public class MyServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        System.out.println("doPost---");
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        System.out.println("doGet---");
+    }
+}
